de15f88ba6e0814a09b27270a727bb610b27461e
mucacran <el.mucacran.rasta@hotmail.com>
2020-12-22 12:45:17 -0500

    avances

--- index.php
+++ index.php
@@ -3,29 +3,43 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ENtrando a la interla</title>
+    <title>Entrando a la interla</title>
     <!--Pilas con botstrap-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-    <h5>Hola soy Sergio</h5>
-    <?php
-        echo '<h1>Al parecer vamos bien</h1>';
-    ?>
-    <?php
+<?php
     //llamando a la libreria de SOUP
-    require_once "lib/nusoap.php";
+        require_once "lib/nusoap.php";
     //conectandome al servidor de Dirnadap
-    $cliente = new nusoap_client('https://canales.cfn.fin.ec:7849/dinardapService?wsdl','wsdl');
+        $cliente = new nusoap_client('https://canales.cfn.fin.ec:7849/dinardapService?wsdl','wsdl');
 
-    $resultado = $cliente->call('getFichaGeneral',array('codigoPaquete'=>'604','numeroIdentificacion'=>'0917045387'));
+        $resultado = $cliente->call('service',array('codigoPaquete'=>'604','numeroIdentificacion'=>'0917045387'));
+?>
+<div class="container">
+    <div class="row">
+        <div class="col-sm-12 col-md-6 border">
+            <?php
+                echo '<pre>';
+                echo var_dump($resultado);
+                //echo var_dump($cliente);
+                echo '</pre>';
 
+                //arreglo asociativo multidimencional
+            ?>  
+        </div>
+        <div class="col-sm-12 col-md-6 border">
+            <?php
+                echo '<pre>';
+                echo var_dump($resultado);
+                //echo print_r($cliente);
+                echo '</pre>';
 
-    echo '<pre>';
-    //echo var_dump($resultado);
-    echo var_dump($cliente);
-    echo '</pre>';
+                //arreglo asociativo multidimencional
+            ?>  
+        </div>
+    </div>
 
-    //arreglo asociativo multidimencional
-    ?>
+</div>
 </body>
 </html>

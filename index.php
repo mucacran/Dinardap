ab54492a321d2acc7057928abb03fc2261d0602f
mucacran <el.mucacran.rasta@hotmail.com>
2020-12-28 14:59:41 -0500

    esta saliendo muy bien

--- index.php
+++ index.php
@@ -19,15 +19,26 @@
     //conectandome al servidor de Dirnadap
         $cliente = new nusoap_client('https://canales.cfn.fin.ec:7849/dinardapService?wsdl','wsdl');
 
-        $resultado = $cliente->call('getFichaGeneral',array('codigoPaquete'=>'604','numeroIdentificacion'=>'0923127740'));
+        $resultado = $cliente->call('getFichaGeneral',array('codigoPaquete'=>'604','numeroIdentificacion'=>'0917045387'));
 ?>
 <div class="container">
     <div class="row">
         <div class="col-sm-12 col-md-6 border">
             <?php
                 echo '<pre>';
-                echo var_dump($resultado);
+                
+                //echo var_dump($resultado);
                 //echo var_dump($cliente);
+                $instituciones = $resultado["return"]["instituciones"];
+                //echo print_r( $instituciones);
+                $registros = $instituciones["datosPrincipales"]["registros"];
+                //echo print_r( $registros);
+                // $cedula = $registros["0"]["valor"];
+                // $nombre = $registros["1"]["valor"];
+                foreach($registros as $i => $registro){
+                    echo $i . " <b>". $registro["campo"] . ":</b> ". $registro["valor"] . "<br><b>codigo: </b>" . $registro["codigo"] . "<br><br>";
+                }
+
                 echo '</pre>';
 
                 //arreglo asociativo multidimencional

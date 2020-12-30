10fac225cfcd0b38567d4940e8217182073b004b
mucacran <el.mucacran.rasta@hotmail.com>
2020-12-30 16:46:12 -0500

    hasta aqui todo me ha salido muy bien

---
+++ formulario-denunciasYreclamo.php
@@ -0,0 +1,54 @@
+<div role="form" class="wpcf7" id="wpcf7-f13335-o1" dir="ltr" lang="es-ES">
+<div class="screen-reader-response"></div>
+<form action="/xxx-denuncias-reclamos-xxx-prueba/#wpcf7-f13335-o1" method="post" class="wpcf7-form" enctype="multipart/form-data" novalidate="novalidate">
+<div style="display: none;">
+<input type="hidden" name="_wpcf7" value="13335">
+<input type="hidden" name="_wpcf7_version" value="5.1.3">
+<input type="hidden" name="_wpcf7_locale" value="es_ES">
+<input type="hidden" name="_wpcf7_unit_tag" value="wpcf7-f13335-o1">
+<input type="hidden" name="_wpcf7_container_post" value="0">
+<input type="hidden" name="_wpcf7cf_hidden_group_fields" value="[]">
+<input type="hidden" name="_wpcf7cf_hidden_groups" value="[]">
+<input type="hidden" name="_wpcf7cf_visible_groups" value="[]">
+<input type="hidden" name="_wpcf7cf_repeaters" value="[]">
+<input type="hidden" name="_wpcf7cf_steps" value="{}">
+<input type="hidden" name="_wpcf7cf_options" value="{&quot;form_id&quot;:13335,&quot;conditions&quot;:[],&quot;settings&quot;:{&quot;animation&quot;:&quot;yes&quot;,&quot;animation_intime&quot;:200,&quot;animation_outtime&quot;:200,&quot;notice_dismissed&quot;:false}}">
+</div>
+<fieldset disabled>
+<div class="cf7-form-control">
+<i class="fa fa-user antesInput" aria-hidden="true"></i><span class="wpcf7-form-control-wrap your-name"><input type="text" name="your-name" value="<?php echo $razonSocial ?>" size="40" class="wpcf7-form-control wpcf7-text "  aria-invalid="false" placeholder="<?php echo $razonSocial ?>"></span>
+<div class="clearfix"></div>
+</div>
+<div class="cf7-form-control">
+<i class="fa fa-address-card-o antesInput" aria-hidden="true"></i><span class="wpcf7-form-control-wrap numero-cedula"><input type="text" name="numero-cedula" value="<?php  echo $cedula ?>" size="40" class="wpcf7-form-control wpcf7-text "  aria-invalid="false" placeholder="<?php echo $cedula ?>"></span>
+<div class="clearfix"></div>
+</div>
+</fieldset>
+<div class="cf7-form-control">
+<i class="fa fa-envelope antesInput" aria-hidden="true"></i><span class="wpcf7-form-control-wrap your-email"><input type="email" name="your-email" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-email  wpcf7-validates-as-email"  aria-invalid="false" placeholder="Su Email *"></span>
+<div class="clearfix"></div>
+</div>
+<div class="cf7-form-control">
+<i class="fa fa-phone antesInput" aria-hidden="true"></i><span class="wpcf7-form-control-wrap tel-179"><input type="tel" name="tel-179" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-tel  wpcf7-validates-as-tel"  aria-invalid="false" placeholder="Número de teléfono o Celular *"></span>
+<div class="clearfix"></div>
+</div>
+<div class="cf7-form-control">
+<i class="fa fa-comments-o antesInput" aria-hidden="true"></i><span class="wpcf7-form-control-wrap your-subject"><input type="text" name="your-subject" value="" size="40" class="wpcf7-form-control wpcf7-text "  aria-invalid="false" placeholder="Asunto *"></span>
+<div class="clearfix"></div>
+</div>
+<div class="cf7-form-control textarea">
+    <span class="wpcf7-form-control-wrap your-message"><textarea name="your-message" cols="40" rows="10" class="wpcf7-form-control wpcf7-textarea "  aria-invalid="false" placeholder="Contenido de la queja denuncia o reclamo *"></textarea></span>
+</div>
+<div class="cf7-form-control">
+<div class="form-group">
+<a href="http://www.cfn.fin.ec/wp-content/uploads/2016/02/formato_reclamos_ago_14.xls" download="">descarga formulario y adjuntarlo (opcional)</a>
+</div>
+<div class="form-group">
+<span class="wpcf7-form-control-wrap denuncias-reclamos-excel"><input type="file" name="denuncias-reclamos-excel" size="40" class="wpcf7-form-control wpcf7-file" accept=".xlsx,.xls" aria-invalid="false"></span>
+</div>
+</div>
+<p><i class="fa fa-paper-plane" aria-hidden="true"></i> <input type="submit" value="Enviar" class="wpcf7-form-control wpcf7-submit"></p>
+<div class="alert alert-primary" role="alert">
+  * este campo es requerido.
+</div>
+<div class="wpcf7-response-output wpcf7-display-none"></div></form></div>
---
+++ index3.php
@@ -0,0 +1,125 @@
+<?php /* Template Name: Denuncias y Reclamos */
+?>
+
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Entrando a la interla</title>
+	<!--CF7-->
+	<link rel='stylesheet' href='https://www.cfn.fin.ec/wp-content/plugins/cf7-conditional-fields/style.css' type='text/css' media='all' />
+	<!--RASTREA SI UN FORMULARIO SE ENVIO POR MEDIO DE GOOGLE ANALYTICS-->
+<script>
+document.addEventListener( 'wpcf7mailsent', function( event ) {
+  ga('send', 'event', 'Contact Form', 'submit');
+}, false );
+</script>
+	
+    <!--Pilas con botstrap-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet">
+	
+</head>
+<body>
+	<div class="container">
+		<h1>
+		cargando el formulario
+		</h1>
+	</div>
+	<div class="container">
+		<div class="row">
+			<div class="col">
+				<form method="post" name="form" action="?"> 
+					<input type="text" placeholder="Ingrese número de cédula" name="cedula"> 
+					<input class="btn btn-primary" type="submit" value="Cargar la pagina"> 
+				</form>
+				<?php
+					if(!empty($_POST['cedula'])){
+					$numeroCedula = $_POST['cedula'];
+					require_once "lib/nusoap.php";
+					$cliente = new nusoap_client('https://canales.cfn.fin.ec:7849/dinardapService?wsdl','wsdl');
+					$resultado = $cliente->call('getFichaGeneral',array('codigoPaquete'=>'604','numeroIdentificacion'=> $numeroCedula ));
+
+					echo '<pre>';
+                	$instituciones = $resultado["return"]["instituciones"];
+                    $registros = $instituciones["datosPrincipales"]["registros"];
+                    $razonSocial;
+                    $cedula;
+                	foreach($registros as $i => $registro){
+                		if($registro["codigo"] == 2){
+                            //echo $i . " <b>". $registro["campo"] . ":</b> ". $registro["valor"] . "<br><b>codigo: </b>" . $registro["codigo"] . "<br><br>";
+                            $razonSocial = $registro["valor"];
+                        }
+                        if($registro["codigo"] == 1){
+                            $cedula = $registro["valor"];
+                        }
+                    }
+                    echo $razonSocial . " - " . $cedula;
+                    echo '</pre>';
+						?>
+<!---->
+<?php include 'formulario-denunciasYreclamo.php'; ?>
+<!---->
+				<?php
+					}
+				else{
+			        echo do_shortcode('[contact-form-7 id="13335" title="Denuncias y Reclamos_copy"]');
+				}
+				?>
+			</div>
+		</div>
+	</div>
+<!--CF7-->
+	<script type="text/javascript">
+document.addEventListener( 'wpcf7mailsent', function( event ) {
+    if ('7410'== event.detail.contactFormId) {
+		var mensaje = document.getElementById('formularioDeSalidaJuridico');
+		mensaje.innerHTML = '<div class="alert alert-success" role="alert">Su mensaje a sido enviado con éxito</div><h4 class="alert-heading">Estamos buscando el mejor producto para ti. Te contactaremos de inmediato.</h4><br><a href="https://www.cfn.fin.ec" class="btn btn-outline-success">SALIR</a><br><p></p>';
+		//https://contactform7.com/dom-events/
+		var cerrarPopUpJuridicaHijo = document.getElementById('wow-modal-close-3');
+		var cerrarPopUpJuridicaPapa = document.getElementById('wow-modal-overlay-3');
+		//selecionando etiqueta
+		var cuerpoBody = document.getElementsByTagName("BODY")[0];
+		var cuerpoHTML = document.getElementsByTagName("HTML")[0];
+		cerrarPopUpJuridicaHijo.style.display = 'none';cerrarPopUpJuridicaPapa.style.display = 'none';cuerpoBody.classList.remove("no-scroll");cuerpoHTML.classList.remove("no-scroll");
+		
+    }
+	if ( '7404' == event.detail.contactFormId ) {
+		var mensaje = document.getElementById('formularioDeSalidaNatural');
+		mensaje.innerHTML = '<div class="alert alert-success" role="alert">Su mensaje a sido enviado con éxito</div><h4 class="alert-heading">Estamos buscando el mejor producto para ti y te contactaremos de inmediato.</h4><br><a href="https://www.cfn.fin.ec" class="btn btn-outline-success">SALIR</a><br><p></p>';
+		//https://contactform7.com/dom-events/
+		//https://contactform7.com/2017/06/07/on-sent-ok-is-deprecated/
+		
+		var cerrarPopUpNaturalHija = document.getElementById('wow-modal-close-2');
+		var cerrarPopUpNaturalMama = document.getElementById('wow-modal-overlay-2');
+		//selecionando etiqueta
+		var cuerpoBody = document.getElementsByTagName("BODY")[0];
+		var cuerpoHTML = document.getElementsByTagName("HTML")[0];
+		
+		cerrarPopUpNaturalHija.style.display = 'none';cerrarPopUpNaturalMama.style.display = 'none';cuerpoBody.classList.remove("no-scroll");cuerpoHTML.classList.remove("no-scroll");
+    }
+}, false );
+</script>
+<script type="text/javascript">
+document.addEventListener( 'wpcf7invalid', function( event ) {
+    if ('7410'== event.detail.contactFormId) {
+		alert ('algunos de los campos no cumplen con las condiciones');
+    }
+	if ( '7404' == event.detail.contactFormId ) {
+		alert ('algunos de los campos no cumplen con las condiciones');
+    }
+}, false );
+</script>
+<script type='text/javascript' src='https://code.jquery.com/jquery-1.12.4.min.js'></script>
+<script type='text/javascript'>
+/* <![CDATA[ */
+var wpcf7 = {"apiSettings":{"root":"https:\/\/www.cfn.fin.ec\/wp-json\/contact-form-7\/v1","namespace":"contact-form-7\/v1"}};
+/* ]]> */
+</script>
+<script type='text/javascript'>
+/* <![CDATA[ */
+var wpcf7cf_global_settings = {"ajaxurl":"https:\/\/www.cfn.fin.ec\/wp-admin\/admin-ajax.php"};
+/* ]]> */
+</script>
+<script type='text/javascript' src='https://www.cfn.fin.ec/wp-content/plugins/cf7-conditional-fields/js/scripts.js'></script>
+</body>
+</html>
